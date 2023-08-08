76cacf1a7b3548c39d4ca2b51dae9b750f8a5f04
Manunggal Sukendro <manunggal.s@gmail.com>
2023-08-08 07:09:37 +0700

    Add project files.

---
+++ GuestBook/GuestBookMethods.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestBook
+{
+    public class WelcomeGuest
+    {
+        public static (string mainGuest, int companionNumbers, string companionNames) getGuestInfo()
+        {
+            // greet guest and ask the name, save the name as mainGuest, to be used for array name later
+            Console.WriteLine("Hi, welcome to the party");
+            Console.Write("What is your name: ");
+            string mainGuest = Console.ReadLine();
+
+            // ask companion numbers
+            Console.Write("How many companions coming with you: ");
+            int companionNumbers = int.Parse(Console.ReadLine());
+
+            // ask companion names
+            Console.Write("Can you provide their name also: ");
+            string companionNames = Console.ReadLine();
+
+            return (mainGuest, companionNumbers, companionNames);
+
+        }
+
+        // method to create guest array
+        public static string[] createGuestArray(string arrayName)
+        {
+            string[] guestCompanionArray = arrayName.Split(',');
+            foreach(string companion in guestCompanionArray)
+            {
+                Console.WriteLine(companion);
+            }
+         
+
+            return guestCompanionArray;
+        }
+    }
+}
---
+++ GuestBook/Program.cs
@@ -0,0 +1,18 @@
+﻿// Guests book app
+
+using GuestBook;
+
+// get guest information (name, how many companions and their names also)
+var guestInfo = WelcomeGuest.getGuestInfo();
+
+string mainGuestName = guestInfo.mainGuest;
+int mainGuestCompanion = guestInfo.companionNumbers;
+string guestCompanion = guestInfo.companionNames;
+
+// create array of guest companions
+string[] guestCompanionArray = WelcomeGuest.createGuestArray(mainGuestName);
+
+// check array
+Console.WriteLine(guestCompanionArray);
+
+
